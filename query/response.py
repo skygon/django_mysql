c8a2ef19cf965302b28b3f1a4d445e4b2f0654c7
skygon <skygoncui@163.com>
2018-04-22 22:13:20 +0800

    update

--- query/response.py
+++ query/response.py
@@ -1,9 +1,20 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 
-def get_all(request):
-    return HttpResponse("Hello! This is the get all API")
+def get_list(request):
+    page_id = request.GET.get('page')
+    if page_id is None:
+        page_id = 1
+        
+    msg = "<p> Hello! this is get list for page %s. </p>" %(page_id)
+    response = HttpResponse()
+    response.write(msg)
+    return response
 
 def get_specific(request, projectId):
     msg = "Hello! This is the get specific API for project %s" %(projectId)
-    return HttpResponse(msg)
+    data = {}
+    data['name'] = "New York"
+    data['location'] = "wall street"
+    response = JsonResponse(data)
+    return response
--- query/urls.py
+++ query/urls.py
@@ -6,6 +6,6 @@ from . import response
 # info/<int:projectId>
 urlpatterns = [
     #path('', views.index, name='index'),
-    path('list/', response.get_all),
+    path('list/', response.get_list),
     path('<int:projectId>/', response.get_specific),
 ]
